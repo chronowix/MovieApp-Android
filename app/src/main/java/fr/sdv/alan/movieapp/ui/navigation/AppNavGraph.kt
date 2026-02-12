e7fd0add517614abd7a651f28220e09083f616eb
chronowix <a.garcia.fern05@gmail.com>
2026-02-12 22:51:46 +0100

    config de moshi pour kotlin

--- app/src/main/java/fr/sdv/alan/movieapp/ui/navigation/AppNavGraph.kt
+++ app/src/main/java/fr/sdv/alan/movieapp/ui/navigation/AppNavGraph.kt
@@ -1,9 +1,13 @@
 package fr.sdv.alan.movieapp.ui.navigation
 
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navArgument
 import fr.sdv.alan.movieapp.ui.screen.detail.DetailScreen
@@ -12,29 +16,49 @@ import fr.sdv.alan.movieapp.ui.screen.home.HomeScreen
 import fr.sdv.alan.movieapp.ui.screen.search.SearchScreen
 
 @Composable
-fun AppNavGraph(){
+fun AppNavGraph(
+    modifier: Modifier = Modifier
+) {
     val navController = rememberNavController()
 
-    NavHost(
-        navController = navController,
-        startDestination = NavRoutes.HOME
-    ){
-        composable(NavRoutes.HOME) {
-            HomeScreen(
-                onMovieClick = { movieId ->
-                    navController.navigate(NavRoutes.detail(movieId))
-                }
-            )
-        }
-        composable(NavRoutes.SEARCH){ SearchScreen() }
-        composable(NavRoutes.FAVORITES){ FavoritesScreen() }
+    val currentRoute = navController.currentBackStackEntryAsState().value?.destination?.route
+    val showBottomBar = currentRoute in listOf(
+        NavRoutes.HOME,
+        NavRoutes.SEARCH,
+        NavRoutes.FAVORITES
+    )
 
-        composable(
-            route = NavRoutes.DETAILS,
-            arguments = listOf(navArgument("movieId") { type = NavType.IntType })
-        ) { backStackEntry ->
-            val movieId = backStackEntry.arguments?.getInt("movieId") ?: 0
-            DetailScreen(movieId = movieId)
+    Scaffold(
+        bottomBar = {
+            if (showBottomBar) {
+                AppBottomBar(navController = navController)
+            }
+        }
+    ) { innerPadding ->
+
+        NavHost(
+            navController = navController,
+            startDestination = NavRoutes.HOME,
+            modifier = modifier.padding(innerPadding) // ✅ utilise innerPadding
+        ) {
+            composable(NavRoutes.HOME) {
+                HomeScreen(
+                    onMovieClick = { movieId ->
+                        navController.navigate(NavRoutes.detail(movieId))
+                    }
+                )
+            }
+
+            composable(NavRoutes.SEARCH) { SearchScreen() }
+            composable(NavRoutes.FAVORITES) { FavoritesScreen() }
+
+            composable(
+                route = NavRoutes.DETAILS,
+                arguments = listOf(navArgument("movieId") { type = NavType.IntType })
+            ) { entry ->
+                val movieId = entry.arguments?.getInt("movieId") ?: 0
+                DetailScreen(movieId = movieId)
+            }
         }
     }
 }
